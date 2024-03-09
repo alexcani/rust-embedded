62d8c2b3990be5ae81e0ecf2bfd06efe53b1d183
Alex Cani <alexcani109@gmail.com>
2024-03-09 01:42:51 +0100

    Use cortex_m to get the hang of it

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,30 @@
 #![no_std]
 #![no_main]
 
-// pick a panicking behavior
-use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// use panic_abort as _; // requires nightly
-// use panic_itm as _; // logs messages over ITM; requires ITM support
-// use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+use panic_halt as _;
 
-use cortex_m::asm;
+use cortex_m::peripheral::{syst::SystClkSource, Peripherals};
 use cortex_m_rt::entry;
+use cortex_m_semihosting::{debug, hprintln};
 
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+    hprintln!("Hello, world!").unwrap();
 
-    loop {
-        // your code goes here
-    }
+    let cp = Peripherals::take().unwrap();
+    let mut systick = cp.SYST;
+    systick.set_clock_source(SystClkSource::Core);
+    systick.set_reload(8_000_000); // 1s
+
+    hprintln!("Waiting for 1 second...").unwrap();
+    systick.clear_current();
+    systick.enable_counter();
+    while !systick.has_wrapped() {}
+    hprintln!("Done!").unwrap();
+
+    // exit QEMU
+    // NOTE do not run this on hardware; it can corrupt OpenOCD state
+    debug::exit(debug::EXIT_SUCCESS);
+
+        loop {}
 }
